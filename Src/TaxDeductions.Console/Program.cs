e5f77d5f50f2d52bb4331e1f7407126f59736790
duho wise <duhowise@gmail.com>
2018-01-25 07:01:11 +0000

    Initial Commit

---
+++ Src/TaxDeductions.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using TaxDeductions.lib;
+
+namespace TaxDeductions.Console
+{
+    class Program
+    {
+        private static decimal _cumulativetax=0.0M;
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter Amount to Calculate Tax");
+            var amount = Convert.ToDecimal(System.Console.ReadLine());
+
+            if (amount<=TaxTable.MaxNormal)
+            {
+                foreach (var tax in TaxTable.GetTaxTable())
+                {
+                    if ( amount >= tax.CumulativeChargeableIncome)
+                    {
+                        _cumulativetax=_cumulativetax+(tax.ChargeableIncome*tax.Rate);
+                    }
+                }
+            }
+            else
+            {
+                _cumulativetax = 0.25M * amount;
+            }
+            System.Console.WriteLine($"Cumulative Tax: {Math.Round(_cumulativetax,2)}");  
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
