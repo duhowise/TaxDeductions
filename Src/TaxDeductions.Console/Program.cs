1dbbab9eb711a1089953ba30f00c497f8ca0df99
duho wise <duhowise@gmail.com>
2018-01-25 10:27:17 +0000

    Refactorings

--- Src/TaxDeductions.Console/Program.cs
+++ Src/TaxDeductions.Console/Program.cs
@@ -12,24 +12,10 @@ namespace TaxDeductions.Console
         {
             System.Console.WriteLine("Enter Amount to Calculate Tax");
             var amount = Convert.ToDecimal(System.Console.ReadLine());
-
-            if (amount<=TaxTable.MaxNormal)
-            {
-                foreach (var tax in TaxTable.GetTaxTable())
-                {
-                    if ( amount >= tax.CumulativeChargeableIncome)
-                    {
-                        _cumulativetax=_cumulativetax+(tax.ChargeableIncome*tax.Rate);
-                    }
-                }
-            }
-            else
-            {
-                _cumulativetax = 0.25M * amount;
-            }
+           var taxVariations=new TaxTable().Taxes;
+            _cumulativetax = TaxTable.GetTaxPayable(amount, taxVariations);
             System.Console.WriteLine($"Cumulative Tax: {Math.Round(_cumulativetax,2)}");  
-
-
+            
             System.Console.ReadLine();
         }
     }
